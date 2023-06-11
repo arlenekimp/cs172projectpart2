22cf3af3fb7f030cac84ccd4c51a435a4a95fc92
mattjonah <50650497+mattjonah@users.noreply.github.com>
2023-06-10 22:59:00 -0700

    flaskv2 finished

---
+++ flaskv2.py
@@ -0,0 +1,90 @@
+## In the terminal, "export FLASK_APP=flask_demo" (without .py)
+## flask run -h 0.0.0.0 -p 8888
+
+import lucene
+import os
+from org.apache.lucene.store import MMapDirectory, SimpleFSDirectory, NIOFSDirectory
+from java.nio.file import Paths
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.document import Document, Field, FieldType
+from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig, IndexOptions, DirectoryReader
+from org.apache.lucene.search import IndexSearcher, BoostQuery, Query
+from org.apache.lucene.search.similarities import BM25Similarity
+from flask import request, Flask, render_template
+
+app = Flask(__name__)
+
+sample_doc = [
+    {
+        'title' : 'A',
+        'context' : 'lucene is a useful tool for searching and information retrieval'
+        },
+    {
+        'title' : 'B',
+        'context' : 'Bert is a deep learning transformer model for encoding textual data'
+    },
+    {
+        'title' : 'C',
+        'context' : 'Django is a python web framework for building backend web APIs'
+    }
+]      
+
+def retrieve(storedir, query):
+    searchDir = NIOFSDirectory(Paths.get(storedir))
+    searcher = IndexSearcher(DirectoryReader.open(searchDir))
+    
+    parser = QueryParser('title', StandardAnalyzer())
+    parsed_query = parser.parse(query)
+
+    topDocs = searcher.search(parsed_query, 10).scoreDocs
+    topkdocs = []
+    for hit in topDocs:
+        doc = searcher.doc(hit.doc)
+        topkdocs.append({
+            "score": hit.score,
+            "username": doc.get("username"),
+            "title": doc.get("title"),
+            "upvotes": doc.get("upvotes"),
+            "text": doc.get("body")
+        })
+    return topkdocs
+    #print(topkdocs)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+    #return 'hello!~!!'
+
+@app.route("/abc")
+def abc():
+    return 'hello alien'
+
+@app.route('/input', methods = ['POST', 'GET'])
+def input():
+    return render_template('input.html')
+
+@app.route('/output', methods = ['POST', 'GET'])
+def output():
+    if request.method == 'GET':
+        return f"Nothing"
+    if request.method == 'POST':
+        form_data = request.form
+        query = form_data['query']
+        print(f"this is the query: {query}")
+        lucene.getVMEnv().attachCurrentThread()
+        docs = retrieve('food', str(query))
+        print(docs)
+        
+        return render_template('output.html',lucene_output = docs)
+    
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)           # port 5000
+    # app.run(host = "127.0.0.1") # port 5000
+    # app.run(host = "8888")
+
+# create_index('sample_lucene_index/')
+# retrieve('sample_lucene_index/', 'web data')
